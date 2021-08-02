4be1804f774bcc1e7a6d031c6899c416945d85ac
jvnlee <lhjun1028@naver.com>
2021-08-02 21:39:31 +0900

    finished Part 6

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -5,9 +5,10 @@ import Loader from "Components/Loader";
 import Section from "Components/Section";
 import Message from "Components/Message";
 import Poster from "Components/Poster";
+import Helmet from "react-helmet";
 
 const Container = styled.div`
-  padding: 0px 20px;
+  padding: 20px;
 `;
 
 const Form = styled.form`
@@ -30,59 +31,66 @@ const SearchPresenter = ({
   handleSubmit,
   updateTerm,
 }) => (
-  <Container>
-    <Form onSubmit={handleSubmit}>
-      <Input
-        placeholder="Look for Movies or TV Shows..."
-        value={searchTerm}
-        onChange={updateTerm}
-      />
-    </Form>
-    {loading ? (
-      <Loader />
-    ) : (
-      <>
-        {movieResults && movieResults.length > 0 && (
-          <Section title={"Movie Results"}>
-            {movieResults.map((movie) => (
-              <Poster
-                key={movie.id}
-                id={movie.id}
-                imageUrl={movie.poster_path}
-                title={movie.original_title}
-                rating={movie.vote_average}
-                isMovie={true}
-                year={movie.release_date && movie.release_date.substring(0, 4)}
-              />
-            ))}
-          </Section>
-        )}
-        {tvResults && tvResults.length > 0 && (
-          <Section title={"TV Show Results"}>
-            {tvResults.map((show) => (
-              <Poster
-                key={show.id}
-                id={show.id}
-                imageUrl={show.poster_path}
-                title={show.name}
-                rating={show.vote_average}
-                year={
-                  show.first_air_date && show.first_air_date.substring(0, 4)
-                }
-              />
-            ))}
-          </Section>
-        )}
-        {error && <Message color="#e74c3c" text={error} />}
-        {tvResults &&
-          movieResults &&
-          tvResults.length === 0 &&
-          movieResults.length === 0 && (
-            <Message text="Nothing found" color="#95a5a6" />
+  <>
+    <Helmet>
+      <title>Search | Junflix</title>
+    </Helmet>
+    <Container>
+      <Form onSubmit={handleSubmit}>
+        <Input
+          placeholder="Look for Movies or TV Shows..."
+          value={searchTerm}
+          onChange={updateTerm}
+        />
+      </Form>
+      {loading ? (
+        <Loader />
+      ) : (
+        <>
+          {movieResults && movieResults.length > 0 && (
+            <Section title={"Movie Results"}>
+              {movieResults.map((movie) => (
+                <Poster
+                  key={movie.id}
+                  id={movie.id}
+                  imageUrl={movie.poster_path}
+                  title={movie.original_title}
+                  rating={movie.vote_average}
+                  isMovie={true}
+                  year={
+                    movie.release_date && movie.release_date.substring(0, 4)
+                  }
+                />
+              ))}
+            </Section>
           )}
-      </>
-    )}
-  </Container>
+          {tvResults && tvResults.length > 0 && (
+            <Section title={"TV Show Results"}>
+              {tvResults.map((show) => (
+                <Poster
+                  key={show.id}
+                  id={show.id}
+                  imageUrl={show.poster_path}
+                  title={show.name}
+                  rating={show.vote_average}
+                  year={
+                    show.first_air_date && show.first_air_date.substring(0, 4)
+                  }
+                />
+              ))}
+            </Section>
+          )}
+          {error && <Message color="#e74c3c" text={error} />}
+          {tvResults &&
+            movieResults &&
+            tvResults.length === 0 &&
+            movieResults.length === 0 && (
+              <Message text="Nothing found" color="#95a5a6" />
+            )}
+        </>
+      )}
+    </Container>
+  </>
 );
 
 SearchPresenter.propTypes = {
